d788c0594e47567af10d0bc5ada05ccb9ff5c9ad
Katsuki Kobayashi <rare@tirasweel.org>
2019-09-17 23:46:29 +0900

    rough implementation for ftyp-atom

--- src/ftyp.rs
+++ src/ftyp.rs
@@ -1,18 +1,43 @@
 #[derive(Debug)]
-pub struct Ftyp<'a> {
+pub struct Ftyp {
     pub size: u32,
-    pub type_str: &'a [u8; 4],
-    pub major_brand: u32,
+    pub type_str: [u8; 4],
+    pub major_brand: [u8; 4],
+    pub minor_version: [u8; 4],
+    pub compatible_brands: Vec<[u8; 4]>,
 }
 
-pub fn parse_ftyp<'a>(input: &'a [u8]) -> Result<Ftyp<'a>, &'static str> {
-    let size = &input[..4];
+pub fn parse_ftyp<'a>(input: &'a [u8]) -> Result<Ftyp, &'static str> {
+    if input.len() < 8 {
+        return Err("input data is too short");
+    }
 
-    println!("{:?}", size);
+    let size = input[..4]
+        .iter()
+        .fold(0, |val: u32, x| (val << 8) + *x as u32);
+
+    let mut type_str: [u8; 4] = [0; 4];
+    type_str.clone_from_slice(&input[4..8]);
+
+    let mut major_brand: [u8; 4] = [0; 4];
+    major_brand.clone_from_slice(&input[8..12]);
+
+    let mut minor_version: [u8; 4] = [0; 4];
+    minor_version.clone_from_slice(&input[12..16]);
+
+    let mut compatible_brands = Vec::<[u8; 4]>::new();
+
+    for i in 0..((size - 16) / 4) {
+        let mut brand: [u8; 4] = [0; 4];
+        brand.clone_from_slice(&input[((4 * i + 16) as usize)..((4 * i + 20) as usize)]);
+        compatible_brands.push(brand);
+    }
 
     Ok(Ftyp {
-        size: 0,
-        type_str: b"ftyp",
-        major_brand: 0,
+        size,
+        type_str,
+        major_brand,
+        minor_version,
+        compatible_brands,
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,37 @@ pub mod ftyp;
 use ftyp::Ftyp;
 
 #[derive(Debug)]
-pub struct AtomFile<'a> {
-    pub ftyp: Ftyp<'a>,
+pub struct AtomFile {
+    pub ftyp: Ftyp,
 }
 
-impl<'a> AtomFile<'a> {
-    pub fn new(input: &'a [u8]) -> Result<AtomFile<'a>, &'static str> {
-        let ftyp = ftyp::parse_ftyp(input);
-        Ok(AtomFile {
-            ftyp: Ftyp {
-                size: 0,
-                type_str: b"ftyp",
-                major_brand: 0,
-            },
-        })
+impl<'a> AtomFile {
+    pub fn new(input: &'a [u8]) -> Result<AtomFile, &'static str> {
+        let ftyp = ftyp::parse_ftyp(input)?;
+        Ok(AtomFile { ftyp })
     }
+}
+
+#[test]
+fn test_ftyp() {
+    let buf = [
+        0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x69, 0x73, 0x6f, 0x6d, 0x00, 0x00, 0x02,
+        0x00, 0x69, 0x73, 0x6f, 0x6d, 0x69, 0x73, 0x6f, 0x32, 0x61, 0x76, 0x63, 0x31, 0x6d, 0x70,
+        0x34, 0x31, 0x00, 0x00, 0x00, 0x08, 0x66, 0x72, 0x65, 0x65, 0x00, 0x00, 0x62, 0x5c, 0x6d,
+        0x64, 0x61, 0x74,
+    ];
+    let atom = AtomFile::new(&buf).unwrap();
+
+    println!("{:#?}", atom);
+
+    assert_eq!(atom.ftyp.size, 0x20);
+    assert_eq!(atom.ftyp.type_str, [b'f', b't', b'y', b'p']);
+    assert_eq!(atom.ftyp.major_brand, [b'i', b's', b'o', b'm']);
+    assert_eq!(atom.ftyp.minor_version, [0x00, 0x00, 0x02, 0x00]);
+
+    assert_eq!(atom.ftyp.compatible_brands.len(), 4);
+    assert_eq!(atom.ftyp.compatible_brands[0], [b'i', b's', b'o', b'm']);
+    assert_eq!(atom.ftyp.compatible_brands[1], [b'i', b's', b'o', b'2']);
+    assert_eq!(atom.ftyp.compatible_brands[2], [b'a', b'v', b'c', b'1']);
+    assert_eq!(atom.ftyp.compatible_brands[3], [b'm', b'p', b'4', b'1']);
 }
