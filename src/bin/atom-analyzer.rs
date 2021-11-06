0a5ea9fd552aa8d7b0d05291961227eb47b33dec
Katsuki Kobayashi <rare@tirasweel.org>
2021-11-06 17:44:38 +0900

    Qtテスト

--- src/bin/atom-analyzer.rs
+++ src/bin/atom-analyzer.rs
@@ -1,8 +1,14 @@
 use std::path::PathBuf;
 
+use atom_analyzer::qtfile::{self, QtFileError};
 use clap::Clap;
 
-use atom_analyzer::qtfile::{self, QtFileError};
+#[cfg(feature = "gui")]
+use qt_core::QBox;
+#[cfg(feature = "gui")]
+use qt_widgets::{QApplication, QWidget};
+#[cfg(feature = "gui")]
+use std::rc::Rc;
 
 #[derive(Clap)]
 #[clap(name=env!("CARGO_PKG_NAME"))]
@@ -11,11 +17,46 @@ struct Opts {
     input: PathBuf,
 }
 
+#[cfg(feature = "gui")]
+struct MainWindow {
+    widget: QBox<QWidget>,
+}
+
+#[cfg(feature = "gui")]
+impl MainWindow {
+    fn new() -> Rc<MainWindow> {
+        let widget = unsafe { QWidget::new_0a() };
+
+        Rc::new(Self { widget })
+    }
+
+    fn show(&self) {
+        unsafe { self.widget.show() };
+    }
+}
+
+#[cfg(feature = "gui")]
+fn run(_file: &qtfile::QtFile) {
+    QApplication::init(|_| unsafe {
+        let main = MainWindow::new();
+
+        main.show();
+
+        QApplication::exec()
+    })
+}
+
+#[cfg(not(feature = "gui"))]
+fn run(file: &qtfile::QtFile) {
+    println!("{:#?}", file);
+}
+
 fn main() -> Result<(), QtFileError> {
     let opts = Opts::parse();
 
     let t = qtfile::parse_file(opts.input)?;
-    println!("{:#?}", t);
+
+    run(&t);
 
     Ok(())
 }
