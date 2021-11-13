ff140872b8c0d77e57393af72859d42a6e773ad1
Katsuki Kobayashi <rare@tirasweel.org>
2021-11-13 17:36:28 +0900

    終了メニューを実装

--- src/bin/atom-analyzer.rs
+++ src/bin/atom-analyzer.rs
@@ -4,9 +4,11 @@ use atom_analyzer::qtfile::{self, QtFileError};
 use clap::Clap;
 
 #[cfg(feature = "gui")]
-use qt_core::{qs, QBox, QPtr};
+use cpp_core::{Ptr, StaticUpcast};
 #[cfg(feature = "gui")]
-use qt_widgets::{QAction, QApplication, QMainWindow, QMenu, QVBoxLayout};
+use qt_core::{qs, slot, QBox, QObject, QPtr, SlotNoArgs};
+#[cfg(feature = "gui")]
+use qt_widgets::{QAction, QApplication, QMainWindow, QMenu};
 #[cfg(feature = "gui")]
 use std::rc::Rc;
 
@@ -22,45 +24,57 @@ struct MainWindow {
     main_window: QBox<QMainWindow>,
 
     menu_file: QPtr<QMenu>,
+    action_quit: QBox<QAction>,
+}
+
+#[cfg(feature = "gui")]
+impl StaticUpcast<QObject> for MainWindow {
+    unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<QObject> {
+        ptr.main_window.as_ptr().static_upcast()
+    }
 }
 
 #[cfg(feature = "gui")]
 impl MainWindow {
     fn new() -> Rc<MainWindow> {
-        let main_window = unsafe { QMainWindow::new_0a() };
+        let this = unsafe {
+            let main_window = QMainWindow::new_0a();
+            let menu_file = main_window.menu_bar().add_menu_q_string(&qs("&File"));
+            let action_quit = QAction::from_q_string(&qs("E&xit"));
 
-        let menu_file = unsafe { main_window.menu_bar().add_menu_q_string(&qs("&File")) };
+            main_window.show();
 
-        let mut this = Self {
-            main_window,
-            menu_file,
+            Rc::new(Self {
+                main_window,
+                menu_file,
+                action_quit,
+            })
         };
 
-        this.init();
-
-        Rc::new(this)
-    }
-
-    fn init(&mut self) {
         unsafe {
-            let action_quit = QAction::from_q_string(&qs("Quit"));
-
-            self.menu_file.add_action(&action_quit);
+            this.init();
         }
+
+        this
     }
 
-    fn show(&self) {
-        unsafe { self.main_window.show() };
+    unsafe fn init(self: &Rc<Self>) {
+        self.menu_file.add_separator();
+        self.menu_file.add_action(&self.action_quit);
+
+        self.action_quit.triggered().connect(&self.slot_on_quit());
+    }
+
+    #[slot(SlotNoArgs)]
+    unsafe fn on_quit(self: &Rc<Self>) {
+        self.main_window.close();
     }
 }
 
 #[cfg(feature = "gui")]
 fn run(_file: &qtfile::QtFile) {
     QApplication::init(|_| unsafe {
-        let main = MainWindow::new();
-
-        main.show();
-
+        let _main = MainWindow::new();
         QApplication::exec()
     })
 }
